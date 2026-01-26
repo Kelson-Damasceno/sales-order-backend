98b2e8bb50ef09d4717ebc68af0389d30d5676ff
Kelson Damasceno <damasceno.kelson10@uni9.edu.br>
2026-01-26 19:26:43 -0300

    feat: add after read customers logic

--- srv/main.ts
+++ srv/main.ts
@@ -1,2 +1,12 @@
-const funcao = (variavel: string) => console.log(variavel);
-funcao('134');
+import { Service } from '@sap/cds';
+import { Customers } from '@cds-models/sales';
+
+export default (service: Service) => {
+    service.after('READ', 'Customers', (results: Customers) => {
+        results.forEach(customer => {
+            if(!customer.email?.includes('@')){
+                customer.email = `${customer.email}@gmail.com`; 
+            }
+        });
+    })
+}
