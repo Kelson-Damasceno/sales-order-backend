a5bb9c55f9a298f168f9491fa598e500315f8e2f
Kelson Damasceno <damasceno.kelson10@uni9.edu.br>
2026-01-27 18:02:40 -0300

    feat: add record validations

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,6 @@
-import { Service } from '@sap/cds';
+import cds, { Request, Service } from '@sap/cds';
 import { Customers } from '@cds-models/sales';
+
 
 export default (service: Service) => {
     service.after('READ', 'Customers', (results: Customers) => {
@@ -7,6 +8,27 @@ export default (service: Service) => {
             if(!customer.email?.includes('@')){
                 customer.email = `${customer.email}@gmail.com`; 
             }
-        });
-    })
+        })
+    });
+    service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
+        const params = request.data;
+        if(!params.customer_id){
+            return request.reject(400, 'Customer inavalido');
+        }
+
+        console.log(params);
+
+        if(!params.items || params.items?.length === 0){
+            return request.reject(400, 'Itens invalidos')
+        }
+
+        const customerQuery = SELECT.one.from('sales.Customers').where({id: params.cutormer_id});
+        const customer = await cds.run(customerQuery);
+        if(!customer){
+            return request.reject(404, 'Customer não encontrado');
+        }
+
+
+        
+    });
 }
