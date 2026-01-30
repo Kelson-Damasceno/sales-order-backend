ae96bde391c656e7936bb1de6d3e91cbd536654b
Kelson Damasceno <damasceno.kelson10@uni9.edu.br>
2026-01-30 14:03:02 -0300

    feat: add discount in totalAmount in ts code

--- srv/main.ts
+++ srv/main.ts
@@ -50,8 +50,11 @@ export default (service: Service) => {
         items.forEach(item =>{
             totalAmount =+ (item.price as number) * (item.quantity as number);
         })
-        console.log(params);
-        console.log(totalAmount);
+        console.log(`Antes do desconto ${totalAmount}`);
+        if(totalAmount > 30000){
+            totalAmount = totalAmount - (totalAmount * (10/100));
+        }
+        console.log(`Depois do desconto ${totalAmount}`);
         request.data.totalAmount = totalAmount;
     });
 
