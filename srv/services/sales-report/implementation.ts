50225a4d4aa81ee1af0ce7672cb3203b892a8f8d
Kelson Damasceno <damasceno.kelson10@uni9.edu.br>
2026-03-09 18:39:48 -0300

    feat: add SalesReportService and SalesReportServiceImpl

---
+++ srv/services/sales-report/implementation.ts
@@ -0,0 +1,17 @@
+import { ExpectedResult as SalesReportByDays } from "@/@cds-models/db/types/SalesReportByDays";
+
+import { SalesReportRepository } from "@/repositories/sales-report/protocols";
+import { SalesReportService } from "@/services/sales-report/protocols";
+
+
+export class SalesReportServiceImpl implements SalesReportService {
+        constructor(private readonly repository: SalesReportRepository){} 
+    
+        public async findByDays(days: 7): Promise<SalesReportByDays[]> {
+         const reportData = await this.repository.findByDays(days);
+         if(!reportData) {
+            return [];
+         }
+         return reportData?.map(r => r.toObject());
+     }
+}
---
+++ srv/services/sales-report/protocols.ts
@@ -0,0 +1,5 @@
+import { ExpectedResult as SalesReportByDays } from '../../@cds-models/db/types/SalesReportByDays';
+
+export interface SalesReportService {
+        findByDays(days: number): Promise<SalesReportByDays []>; 
+}
