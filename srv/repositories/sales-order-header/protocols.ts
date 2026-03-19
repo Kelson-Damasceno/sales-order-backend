0a2a07eda9357e548e1030b9882b612cfc858696
Kelson Damasceno <damasceno.kelson10@uni9.edu.br>
2026-03-19 14:44:33 -0300

    feat: create SalesOrderHeaderRepository and SalesOrderHeaderRepositoryImpl

---
+++ srv/repositories/sales-order-header/protocols.ts
@@ -0,0 +1,6 @@
+import { SalesOrderHeaderModel } from "@/models/sales-order-header";
+
+export interface SalesOrderHeaderRepository {
+    bulkCreate(headers: SalesOrderHeaderModel[]): Promise<void>;
+}
+
